f3b5b229ecf00c8a146e1d4bc3393dd8ed558b7b
Alvelo <jesusalejandroap11@gmail.com>
2024-01-21 13:04:04 -0400

    End Section 4 - Router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { RegisterComponent } from './auth/register/register.component';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { LoginComponent } from './auth/login/login.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { PagesComponent } from './pages/pages.component';
+import { NopagefoundComponent } from './auth/nopagefound/nopagefound.component';
+import { PagesModule } from './pages/pages.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    BreadcrumbsComponent,
-    HeaderComponent,
-    SidebarComponent,
-    ProgressComponent,
-    Graficas1Component,
-    PagesComponent
+    NopagefoundComponent,   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
