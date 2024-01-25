f3916001c185f1385b4b68e979f758a385be09a9
Alvelo <jesusalejandroap11@gmail.com>
2024-01-25 19:47:25 -0400

    Added css classes without ngClass

--- src/app/pages/account-setting/account-setting.component.ts
+++ src/app/pages/account-setting/account-setting.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-account-setting',
   templateUrl: './account-setting.component.html',
   styles: ``
 })
-export class AccountSettingComponent {
+  export class AccountSettingComponent implements OnInit {
+    public linkTheme = document.querySelector('#theme');
+    public links!: NodeListOf<Element>;
+  
+    constructor(){}
+  ngOnInit(): void {
+    this.links = document.querySelectorAll('.selector');
+    this.checkCurrentTheme();
+    
+  }
 
+  changeTheme(theme:string) {
+
+   
+    const url = `./assets/css/colors/${theme}.css`;
+    // this attribute is part of vanilla javascript
+    this.linkTheme?.setAttribute('href',url);
+
+    localStorage.setItem('theme url', url);
+    
+    this.checkCurrentTheme()
+
+  }
+
+  checkCurrentTheme() {
+
+    this.links.forEach(element => {
+      element.classList.remove('working');
+      const btnTheme = element.getAttribute('data-theme');
+      const btnThemeUrl = `./assets/css/colors/${btnTheme}.css`;
+      const currentTheme = this.linkTheme?.getAttribute('href')
+
+      if (btnThemeUrl === currentTheme){
+        element.classList.add('working');
+
+      }
+    })
+    
+  }
 }
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pages',
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.css'
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
+  
+  public linkTheme = document.querySelector('#theme');
+
+  ngOnInit(): void {
+    const url = localStorage.getItem('theme url') || './assets/css/colors/purple-dark.css';
+    this.linkTheme?.setAttribute('href',url);
+  }
 
 }
